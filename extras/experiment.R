c958a2a2f8c2c8bedcf6af41e175bfc12b42d0a7
amanigaultw <106684916+AIDA64Doc@users.noreply.github.com>
2022-09-09 16:28:01 -0400

    Update experiment.R

--- extras/experiment.R
+++ extras/experiment.R
@@ -14,7 +14,7 @@ toa_ref <- get_toa_ref(gene_symbols = epith_mesen_ref[,1],
                        exp_control = epith_mesen_ref[,12:21])
 
 #get differentially expressed genes as a function of the stress predictor
-DEG_result <- get_DEG(x = rep(1,6),
+DEG_result <- get_DEG(x = Chang$stress,
                       genes = subset(Chang, select = -stress),
                       foldThreshDEG = 1.25)
 
