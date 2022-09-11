f79005edc58e736ea3557464094a06fe4b9ebf2b
amanigaultw <106684916+AIDA64Doc@users.noreply.github.com>
2022-09-11 16:37:57 -0400

    added and documented decouple_gene_symbol function

---
+++ R/decouple_multi_gene_rows.R
@@ -0,0 +1,67 @@
+#' decouple gene expression matrices/data frame where some rows correspond to multiple gene symbols
+#'
+#' \code{decouple_multi_gene_rows} produces a data frame containing one row per gene symbols, where expression values are identical for previously coupled symbols.
+#'
+#' @param df a data frame or matrix containing gene symbols and gene expression values.
+#' @param sep a string indicating how multi-gene symbols are separated (e.g., "///").
+#' @param gene_col a numeric value indicating the position of the gene symbol column within \code{df}; the first column is used by default.
+#' @param header bool indicating whether the first row of data is a header; TRUE by default.
+#' @param progress bool indicating whether to display function progress.
+#' @return a data frame containing one row per gene symbols, where expression values are identical for previously coupled symbols.
+#' @examples
+#' #load example data
+#' data("HumanCD14CD16NegVsPosVsDC_3Reps_Ziegler")
+#' #decouple_multi_gene_rows
+#' decoupled_data <- decouple_multi_gene_rows(HumanCD14CD16NegVsPosVsDC_3Reps_Ziegler)
+#' @export
+decouple_multi_gene_rows <- function(df, sep = "///", gene_col = 1, header = TRUE, progress = TRUE){
+
+  df <- as.data.frame(df)
+
+  #store header row
+  if(header){
+    temp_row <- df[1,]
+    df <- df[-1,]
+  }
+
+  #set expression columns to numeric
+  df[,-gene_col] <- sapply(df[,-gene_col], as.numeric)
+  #get rid of empty gene symbol rows
+  df <- subset(df, df[,gene_col] != "")
+
+  #decouple multi-gene rows
+  temp <- list()
+  for(i in 1:nrow(df)){
+    symbol <- df[i,gene_col]
+
+    if(grepl(sep, symbol)){
+      symbols <- unlist(strsplit(symbol, sep, fixed = TRUE)) #generate vector of gene symbols
+      symbols <- gsub(" ", "", symbols) #get rid of spaces in gene symbol names
+      m <- matrix(nrow = length(symbols), ncol = ncol(df))
+      for(j in 1:length(symbols)){
+        m[j,] <- c(symbols[j], t(df[i,-gene_col]))
+      }
+      temp[[i]] <- as.data.frame(m)
+    }else{
+      temp[[i]] <- as.data.frame(df[i,])
+    }
+
+    if(progress == TRUE){
+      p <- i / nrow(df) * 100
+      svMisc::progress(p)
+    }
+  }
+
+  #generate a dataframe from the list
+  df_temp <- do.call(rbind, temp)
+
+  #restore header
+  if(header){
+    df_temp <- rbind(temp_row, df_temp)
+    row.names(df_temp) <- NULL
+  }
+
+  return(df_temp)
+}
+
+
