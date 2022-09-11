547eaf7eaca0827e64508c0d6fe5c5c0076ad459
amanigaultw <106684916+AIDA64Doc@users.noreply.github.com>
2022-09-11 15:09:30 -0400

    Update experiment.R

--- extras/experiment.R
+++ extras/experiment.R
@@ -51,13 +51,26 @@ data("Chang")
 data("toa_ref_epith_mesen")
 
 #get differentially expressed genes
-DEG_result <- get_DEG(x = Chang$stress, genes = subset(Chang, select = -stress), foldThreshDEG = 1.25)
+DEG_result <- get_DEG(x = Chang$stress,
+                      genes = subset(Chang, select = -stress),
+                      foldThreshDEG = 1.25)
 table(DEG_result$DEG)
 
+#get diagnosticity scores
+toa_ref_epith_mesen <- get_toa_ref(gene_symbols = epith_mesen_ref_raw[,1],
+                                   exp_treatment = epith_mesen_ref_raw[,2:11],
+                                   exp_control = epith_mesen_ref_raw[,12:21])
+
 #toa
-toa_result <- toa(x = Chang$stress, genes = subset(Chang, select = -stress), toa_ref = toa_ref_epith_mesen, foldThreshDEG = 1.25)
+toa_result <- toa(x = Chang$stress,
+                  genes = subset(Chang, select = -stress),
+                  toa_ref = toa_ref_epith_mesen,
+                  foldThreshDEG = 1.25)
 toa_result$df_results
 
 #toa_boot
-toa_boot_result <- toa_boot(x = Chang$stress, genes = subset(Chang, select = -stress), toa_ref = toa_ref_epith_mesen, foldThreshDEG = 1.25)
+toa_boot_result <- toa_boot(x = Chang$stress,
+                            genes = subset(Chang, select = -stress),
+                            toa_ref = toa_ref_epith_mesen,
+                            foldThreshDEG = 1.25)
 toa_boot_result$df_results
