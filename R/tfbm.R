a7b956b58d4d17cce894b23be6d84c3ddf4b66a5
amanigaultw <106684916+amanigaultw@users.noreply.github.com>
2022-09-13 14:09:18 -0400

    updated tfbm doc

--- R/tfbm.R
+++ R/tfbm.R
@@ -3,7 +3,7 @@
 #' performs transcript origin analysis for Transcription Factor Binding Motif without bootstrapped estimates of mean diagnosticity scores.
 #'
 #' @inheritParams get_DEG
-#' @param tfbm_ref a list object containing several tfbm data frames
+#' @param tfbm_ref a list object containing several tfbm reference data frames; the first column of each data frame must contain gene symbols and be named "gene"
 #' @return tfbm returns an object of class "tfbm"
 #'
 #' An object of class "tfbm" is a list containing:
@@ -16,10 +16,10 @@
 #' @examples
 #' #load example data
 #' data("Chang")
-#' 
+#'
 #' #load a tfbm database from another repo (because it is >27MB)
 #' #Rfssa::load_github_data("https://github.com/amanigaultw/TELiS/blob/main/HumanTransfacTELiS2019.RData")
-#' 
+#'
 #' #tfbm
 #' #tfbm_result <- tfbm(x <- Chang[,1],
 #' #                    genes <- Chang[,-1],
@@ -28,49 +28,49 @@
 #' #                    foldThreshDEG = 1.25)
 #' @export
 tfbm <- function(x, genes, tfbm_ref, cov = NULL, foldThreshDEG = 1.5){
-  
+
   #instantiate tfbm result object
   results <- list(df_results = NULL,
                   df_DEG = NULL,
                   df_ratios = NULL,
                   inputs = as.list(environment()))
   class(results) <- "tfbm"
-  
+
   #check inputs
   if(!valid_input_x(x) | !valid_input_genes(genes) | !valid_input_cov(cov) | !valid_input_tfbm_ref(tfbm_ref)){
     warning("invalid inputs; check warnings")
     return(results)
   }
-  
+
   #get differentially expressed genes
   df_DEG = get_DEG(x, genes, cov, foldThreshDEG)
-  
+
   if(is.null(df_DEG)){
     warning("get_DEG failed")
     return(results)
   }
-  
+
   #get tfbm ratios (as up-reg over down-reg)
   df_ratios <- get_ratios(df_DEG, tfbm_ref)
-  
+
   if(is.null(df_ratios)){
     warning("get_ratios failed")
     return(results)
   }
-  
+
   nValid <- apply(df_ratios, 2, function(x) sum(!is.na(x)))
   log2mean <- apply(df_ratios, 2, function(x) mean(log2(x), na.rm = TRUE))
   mean <- 2^log2mean
-  
+
   df_results <- data.frame(TFBM = colnames(df_ratios),
                            n_valid_ratios = nValid,
                            mean_fold_dif = mean,
                            log2_mean_fold_diff = log2mean)
-  
+
   #if no failure up to this point, update the results list with computed values
   results$df_results <- df_results
   results$df_DEG <- df_DEG
   results$df_ratios <- df_ratios
-  
+
   return(results)
 }
