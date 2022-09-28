5c272b5fdb3838e3b5e69e1d209efdf5b4dfb030
andrewmanigaultw <andrewmanigaultw@gmail.com>
2022-09-28 15:49:37 -0400

    Create test-get_DEG.R

---
+++ tests/testthat/test-get_DEG.R
@@ -0,0 +1,15 @@
+test_that("get_DEG works", {
+
+  data("Chang")
+  DEG_result <- get_DEG(x = Chang$stress,
+                        genes = subset(Chang, select = -stress),
+                        foldThreshDEG = 1.25)
+  temp <- table(DEG_result$DEG)
+  names(temp) <- NULL
+  downreg_count <- temp[1]
+  upreg_count <- temp[3]
+
+  expect_equal(downreg_count, 123)
+
+  expect_equal(upreg_count, 227)
+})
